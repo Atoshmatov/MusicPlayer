56b90adc792ad0789b2247da6fad2127e7344f3e
Developer <alimardontoshmatov51>
2022-06-08 21:38:13 +0500

    add

--- app/src/main/java/uz/gita/musicplayer/mobdev20/presentation/ui/screen/SplashScreen.kt
+++ app/src/main/java/uz/gita/musicplayer/mobdev20/presentation/ui/screen/SplashScreen.kt
@@ -8,11 +8,14 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import by.kirich1409.viewbindingdelegate.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import uz.gita.musicplayer.mobdev20.BuildConfig
 import uz.gita.musicplayer.mobdev20.R
+import uz.gita.musicplayer.mobdev20.databinding.ScreenSplashBinding
 import uz.gita.musicplayer.mobdev20.utils.MyAppManager
 import uz.gita.musicplayer.mobdev20.utils.checkPermissions
 import uz.gita.musicplayer.mobdev20.utils.getMusicCursor
@@ -20,7 +23,10 @@ import uz.gita.musicplayer.mobdev20.utils.getMusicCursor
 @SuppressLint("CustomSplashScreen")
 @AndroidEntryPoint
 class SplashScreen : Fragment(R.layout.screen_splash) {
+    private val binding by viewBinding(ScreenSplashBinding::bind)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.textVersion.text =
+            resources.getString(R.string.text_splash_music, BuildConfig.VERSION_NAME)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             requireActivity().window.navigationBarColor =
                 ContextCompat.getColor(requireContext(), R.color.purple_200)
